96d6e3dfb3027cd3a77bc47119de2b532e005c2f
annasvst <annaatalay@hotmail.com>
2026-09-22 19:22:01 +0200

    Update lesson 30 starter files

--- Lesson30/quotes-app/src/app/page.jsx
+++ Lesson30/quotes-app/src/app/page.jsx
@@ -1,8 +1,8 @@
 'use client';
 
 import { QuoteText } from '@/components/typography/QuoteText';
-import {Subtitle} from '@/components/typography/Subtitle';
-import {Button} from '@/components/Button';
+import { Subtitle } from '@/components/typography/Subtitle';
+import { Button } from '@/components/Button';
 // I did not change any configurations to make imports work, but I noticed tsconfig file was created in the project (I accidentally named on file .tsx = used typescript) which must have resulted in a conflic between jsconfig and tsconfig and lead to @/ not working.
 import { quotes as intialQuotes } from '@/quotes';
 import { useState } from 'react';
@@ -17,14 +17,23 @@ export default function Home() {
     setIndex((prevIndex) => (prevIndex += 1));
   }
 
+  function handleLike() {
+    // noop placeholder
+  }
+
   return (
     <main className='flex flex-col items-center justify-center grow'>
       <div className='text-center bg-zinc-800 p-10 rounded-md'>
+        <div className='flex justify-end mb-6'>
+          <Button title='❤️' handleClick={handleLike} />
+        </div>
         <div className='pb-6'>
           <QuoteText className='text-zinc-100'>{quotes[index].quote}</QuoteText>
           <Subtitle>{quotes[index].author}</Subtitle>
         </div>
-        <Button title='Next quote' handleClick={handleClick} />
+        <div className='flex justify-center'>
+          <Button title='Next quote' handleClick={handleClick} />
+        </div>
       </div>
     </main>
   );
